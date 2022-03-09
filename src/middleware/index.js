719967f2ae9eceedb652e6959d56d65590b1a072
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-03-09 11:55:08 +0200

    Install helmet  and use it R #33

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const compression = require('compression');
+const { hidePoweredBy } = require('helmet');
 const { bankRouter, donorRouter } = require('../routes');
 
 module.exports = (app) => {
   app.use(compression());
+  app.use(hidePoweredBy());
   app.use(express.static('public'));
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
   app.use(bankRouter);
   app.use(donorRouter);
-
 };
