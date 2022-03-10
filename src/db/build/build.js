e6a83b62d30103937a358672b7f318422c4c0ec8
abedshamia <ab.shamia96@gmail.com>
2022-03-10 09:45:19 +0200

    Read and execute the SQL file in a better way using stream

--- src/db/build/build.js
+++ src/db/build/build.js
@@ -3,20 +3,22 @@
 const fs = require('fs');
 const connection = require('../config/db');
 
-// Read the Build.sql file
-const sqlBuildQuery = fs.readFileSync('src/db/build/build.sql', 'utf8');
+// read a file as a stream
+const stream = fs.createReadStream('src/db/build/build.sql', 'utf8');
 
-// Execute the Build.sql file
-connection.query(sqlBuildQuery, (err) => {
-  if (err) {
-    return console.error('Error making query', err);
-  }
-
-  return console.log(
-    'Banks Table was successfully Built',
-    'Donors Table was successfully Built',
-  );
+connection.on('error', err => {
+  console.log('Error connecting to the database', err);
 });
 
-// end connection to database
-connection.end();
+stream.on('data', data => {
+  connection.query(data, err => {
+    if (err) {
+      console.log('Error running query', err);
+    }
+  });
+});
+
+stream.on('end', () => {
+  console.log('Tables created successfully');
+  connection.end();
+});
